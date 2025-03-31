59154aaf9766aaaec36c99292d19698c149ba9ad
Petr5 <user@DESKTOP-DMM07FF.>
2025-03-31 15:50:22 +0300

    add logo

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,23 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+import "./index.css";
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
+const app = document.querySelector<HTMLDivElement>("#app");
+
+if (app) {
+  app.innerHTML = `
+    <div class="flex justify-center items-center h-screen bg-gray-200">
+      <div class="grid grid-cols-8 w-64 h-64 border-4 border-gray-800">
+        ${Array.from({ length: 64 })
+          .map(
+            (_, i) => `
+            <div class="w-8 h-8 ${
+              (Math.floor(i / 8) + (i % 8)) % 2 === 0 ? "bg-white" : "bg-gray-800"
+            }"></div>
+          `
+          )
+          .join("")}
+      </div>
     </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
-
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+  `;
+} else {
+  console.error("Элемент #app не найден!");
+}
